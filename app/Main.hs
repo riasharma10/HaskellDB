7a20334a7e135457e453a930ba5db9cdd72c6ef4
Ria Sharma <riassharma10@gmail.com>
2023-12-17 17:23:26 -0500

    main loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,38 @@
 module Main where
 
+import Control.Monad.IO.Class
+import Control.Monad.State
+import Data.IntMap (update)
+import Data.Map qualified as Map
 import Execute
+import GHC.IO.Device (IODevice (dup))
 import StatementParser
+import Types
+
+initializeEmptyDB :: IO Database
+initializeEmptyDB = do
+  let initialDb = Database Map.empty Map.empty
+  return initialDb
+
+processStatement :: Statement -> StateT Database IO ()
+processStatement statement = do
+  outcome <- executeStatement statement
+  liftIO $ print outcome
+
+mainLoop :: StateT Database IO ()
+mainLoop = do
+  liftIO $ putStr "HaskellDB> "
+  command <- liftIO getLine
+  case command of
+    ":quit" -> liftIO $ putStrLn "Exiting Haskell Database Engine."
+    _ -> do
+      case parseSQL command of
+        Left err -> liftIO $ print err
+        Right statement -> processStatement statement
+      mainLoop
 
 main :: IO ()
 main = do
   putStrLn "Welcome to the Haskell Database Engine!"
-  --   db <- emptyDB
-
-  putStr ("HaskellDB" ++ "> ")
-  str <- getLine
-  case parseSQL str of
-    Left err -> do
-      putStrLn err
-    Right statement -> do
-      print statement
-
---   db' <- execStatement db statement
-
---   repl db'
-
---   repl emptyDB
+  db <- initializeEmptyDB
+  void $ runStateT mainLoop db
