5474fd63d269245b794cb1403ae1ef8b082702cb
Ria Sharma <riassharma10@gmail.com>
2023-12-17 22:45:07 -0500

    main file

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,34 @@
 module Main where
 
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
+import Control.Monad.State
+import Data.IntMap (update)
+import Data.Map qualified as Map
 import Execute
+import GHC.IO.Device (IODevice (dup))
 import StatementParser
+import Types
+
+processStatement :: DBRef -> Statement -> IO ()
+processStatement tdb statement = do
+  outcome <- atomically $ executeStatement tdb statement
+  print outcome
+
+mainLoop :: DBRef -> IO ()
+mainLoop tdb = do
+  putStr "HaskellDB> "
+  command <- getLine
+  case command of
+    ":quit" -> putStrLn "Exiting Haskell Database Engine."
+    _ -> do
+      case parseSQL command of
+        Left err -> print err
+        Right statement -> processStatement tdb statement
+      mainLoop tdb
 
 main :: IO ()
 main = do
   putStrLn "Welcome to the Haskell Database Engine!"
-  --   db <- emptyDB
-
-  putStr ("HaskellDB" ++ "> ")
-  str <- getLine
-  case parseSQL str of
-    Left err -> do
-      putStrLn err
-    Right statement -> do
-      print statement
-
---   db' <- execStatement db statement
-
---   repl db'
-
---   repl emptyDB
+  dbRef <- atomically emptyDB
+  mainLoop dbRef
